ba64ba18c6f99e77d56208816a229d3f0067591d
rkm-1234 <rakesh@influxiqtech.com>
2023-10-04 02:16:44 +0530

    pull

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpStatus, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Req, Res } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { ArtistBody } from 'src/entities/artist-body.entity';
@@ -10,7 +10,7 @@ import { ArtistService } from './artist.service';
 export class ArtistController {
     constructor(private artistService:ArtistService ){}
 
-    @ApiOperation({summary:'Artist'})
+    @ApiOperation({summary:'Add Artist'})
     @Post("/add")
     async addArtist(@Body() body:ArtistBody, @Req() request:FastifyRequest, @Res() reply:FastifyReply){
         try {
@@ -33,8 +33,31 @@ export class ArtistController {
         }
     };
 
+    @ApiOperation({summary:'Artist Find'})
+    @Get("/find")
+    async findArtist(@Body() {_id}:ArtistBody, @Req() request:FastifyRequest, @Res() reply:FastifyReply){
+        try {
+            const data = await this.artistService.findArtist(_id);
+            reply.status(HttpStatus.OK)
+            .header('content-type', 'application/json')
+            .send({
+                "status": "success",
+                "response":{"artistData": data}
+            })
+        } catch (error) {
+            console.log('error', error);
+            reply
+            .status(HttpStatus.BAD_REQUEST)
+            .header('Content_Type','application/json')
+            .send({
+                'status':'error',
+                "response":{"message": error}
+            })
+        }
+    };
 
-    @ApiOperation({summary:'Artist'})
+
+    @ApiOperation({summary:'Update Artist'})
     @Post("/update")
     async updateArtist(@Body() {_id,...field}:ArtistBody, @Req() request:FastifyRequest, @Res() reply:FastifyReply){
         try {
