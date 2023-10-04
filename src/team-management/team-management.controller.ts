f86fb0d3020f7d16cb792a8c193b1c329cb0667f
saumya-dev007 <saumyadev.005@gmail.com>
2023-10-04 22:42:47 +0530

    Team listing API created

--- src/team-management/team-management.controller.ts
+++ src/team-management/team-management.controller.ts
@@ -4,57 +4,91 @@ import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { TeamBody } from 'src/entities/team-body.entity';
 import { FastifyReply, FastifyRequest } from 'fastify';
 
-
-@ApiTags("Team Management")
+@ApiTags('Team Management')
 @Controller('team-management')
 export class TeamManagementController {
-    constructor(private teamService: TeamManagementService){}
+  constructor(private teamService: TeamManagementService) {}
 
-    @ApiOperation({summary:"Add Team Memeber"})
-    @Post("/add")
-    async addTeam(@Body() body:TeamBody, @Req() requist:FastifyRequest, @Res() reply:FastifyReply){
-        try {
-            const data = await this.teamService.addTeam(body);
-            reply
-            .status(HttpStatus.OK)
-            .header('content-type', 'application/json')
-            .send({
-                "status": "success",
-                "response":{"teamData": data}
-            })
-        } catch (error) {
-            console.log('error', error);
-            reply
-            .status(HttpStatus.BAD_REQUEST)
-            .header('content-type', 'application/json')
-            .send({
-                "status":"error",
-                "response":{"message":error}
-            })
-        }
+  @ApiOperation({ summary: 'Add Team Memeber' })
+  @Post('/add')
+  async addTeam(
+    @Body() body: TeamBody,
+    @Req() requist: FastifyRequest,
+    @Res() reply: FastifyReply,
+  ) {
+    try {
+      const data = await this.teamService.addTeam(body);
+      reply
+        .status(HttpStatus.OK)
+        .header('content-type', 'application/json')
+        .send({
+          status: 'success',
+          response: { teamData: data },
+        });
+    } catch (error) {
+      console.log('error', error);
+      reply
+        .status(HttpStatus.BAD_REQUEST)
+        .header('content-type', 'application/json')
+        .send({
+          status: 'error',
+          response: { message: error },
+        });
     }
+  }
 
-
-    @ApiOperation({summary:'Update Team'})
-    @Post("/update")
-    async updateTeam(@Body() {_id,...field}:TeamBody, @Req() request:FastifyRequest, @Res() reply:FastifyReply){
-        try {
-            const data = await this.teamService.updateTeam(_id,field);
-            reply.status(HttpStatus.OK)
-            .header('content-type', 'application/json')
-            .send({
-                "status": "success",
-                "response":{"teamData": data}
-            })
-        } catch (error) {
-            console.log('error', error);
-            reply
-            .status(HttpStatus.BAD_REQUEST)
-            .header('Content_Type','application/json')
-            .send({
-                'status':'error',
-                "response":{"message": error}
-            })
-        }
+  @ApiOperation({ summary: 'Update Team' })
+  @Post('/update')
+  async updateTeam(
+    @Body() { _id, ...field }: TeamBody,
+    @Req() request: FastifyRequest,
+    @Res() reply: FastifyReply,
+  ) {
+    try {
+      const data = await this.teamService.updateTeam(_id, field);
+      reply
+        .status(HttpStatus.OK)
+        .header('content-type', 'application/json')
+        .send({
+          status: 'success',
+          response: { teamData: data },
+        });
+    } catch (error) {
+      console.log('error', error);
+      reply
+        .status(HttpStatus.BAD_REQUEST)
+        .header('Content_Type', 'application/json')
+        .send({
+          status: 'error',
+          response: { message: error },
+        });
     }
+  }
+
+  @ApiOperation({ summary: 'Team Listing' })
+  @Post('/listing')
+  async teamListing(
+    @Req() request: FastifyRequest,
+    @Res() reply: FastifyReply,
+  ) {
+    try {
+      const data = await this.teamService.fetchTeamLisitng({});
+      reply
+        .status(HttpStatus.OK)
+        .header('content-type', 'application/json')
+        .send({
+          status: 'success',
+          response: { teamData: data },
+        });
+    } catch (error) {
+      console.log('error', error);
+      reply
+        .status(HttpStatus.BAD_REQUEST)
+        .header('Content_Type', 'application/json')
+        .send({
+          status: 'error',
+          response: { message: error },
+        });
+    }
+  }
 }
--- src/team-management/team-management.service.ts
+++ src/team-management/team-management.service.ts
@@ -5,30 +5,35 @@ import { ITeam } from 'src/models/team/team.interface';
 
 @Injectable()
 export class TeamManagementService {
-    constructor(@InjectModel('team_lists') private teamModel:Model<ITeam>){}
+  constructor(@InjectModel('team_lists') private teamModel: Model<ITeam>) {}
 
-    async addTeam(data:any):Promise<any>{
-        try {
-            const team = this.teamModel.create(data);
-            return Promise.resolve(team);
-        } catch (error) {
-            return Promise.reject(error)
-        }
+  async addTeam(data: any): Promise<any> {
+    try {
+      const team = this.teamModel.create(data);
+      return Promise.resolve(team);
+    } catch (error) {
+      return Promise.reject(error);
     }
+  }
 
-
-
-    async updateTeam(_id: string, field: any):Promise<any>{
-        try {
-            const team = this.teamModel.findByIdAndUpdate(
-                { _id: new mongoose.Types.ObjectId(_id) },
-                { $set: { ...field } },
-              );
-            return Promise.resolve(team);
-        } catch (error) {
-            return Promise.reject(error)
-        }
+  async updateTeam(_id: string, field: any): Promise<any> {
+    try {
+      const team = this.teamModel.findByIdAndUpdate(
+        { _id: new mongoose.Types.ObjectId(_id) },
+        { $set: { ...field } },
+      );
+      return Promise.resolve(team);
+    } catch (error) {
+      return Promise.reject(error);
     }
+  }
 
-
+  async fetchTeamLisitng(data: any): Promise<any> {
+    try {
+      const teamListingData = this.teamModel.find({});
+      return Promise.resolve(teamListingData);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
 }
